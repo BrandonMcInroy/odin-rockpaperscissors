e661d7b49555ef131b590dbdbb3451854380912d
Brandon McInroy <brandon@codingbusdriver.ca>
2024-08-31 13:36:40 -0700

    Add human choice option, still working on it

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -4,3 +4,24 @@ function getComputerChoice() {
   return "The computer chose" + " " + choice + ".";
 }
 console.log(getComputerChoice());
+
+function getHumanChoice() {
+  const humanChoice = prompt("Please choose Rock, Paper or Scissors");
+
+  if (humanChoice === "rock") {
+    alert("You chose Rock");
+  } else if (humanChoice === "paper") {
+    alert("You chose Paper");
+  } else if (humanChoice === "scissors") {
+    alert("You chose Scissors");
+  } else if (humanChoice === null || humanChoice === " ") {
+    prompt("You must chose Rock, Paper or Scissors");
+  } else {
+    prompt(
+      "That is an invalid choice, please chose only Rock, Paper or Scissors"
+    );
+  }
+  return "You chose" + " " + humanChoice + ".";
+}
+
+console.log(getHumanChoice());
