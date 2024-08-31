265b67adb3a602be41a921f19d3df713f8924286
Brandon McInroy <brandon@codingbusdriver.ca>
2024-08-31 13:58:35 -0700

    If user input === to inlcude all options on 1 line

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -8,18 +8,14 @@ console.log(getComputerChoice());
 function getHumanChoice() {
   const humanChoice = prompt("Please choose Rock, Paper or Scissors");
 
-  if (humanChoice === "rock") {
-    alert("You chose Rock");
-  } else if (humanChoice === "paper") {
-    alert("You chose Paper");
-  } else if (humanChoice === "scissors") {
-    alert("You chose Scissors");
+  if (
+    humanChoice === "rock" ||
+    humanChoice === "paper" ||
+    humanChoice === "scissors"
+  ) {
+    alert("You chose" + " " + humanChoice + ".");
   } else if (humanChoice === null || humanChoice === " ") {
     prompt("You must chose Rock, Paper or Scissors");
-  } else {
-    prompt(
-      "That is an invalid choice, please chose only Rock, Paper or Scissors"
-    );
   }
   return "You chose" + " " + humanChoice + ".";
 }
