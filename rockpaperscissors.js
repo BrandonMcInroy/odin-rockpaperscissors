ac50858900d2eec240c946f93ddbab39df156102
Brandon McInroy <brandon@codingbusdriver.ca>
2024-08-31 22:36:24 -0700

    Add playGame function, working on implimentation

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,6 +1,3 @@
-const humanScore = 0;
-const computerScore = 0;
-
 function getHumanChoice() {
   const humanChoice = prompt("Please choose Rock, Paper or Scissors");
   return humanChoice.toLowerCase();
@@ -15,26 +12,30 @@ function getComputerChoice() {
 }
 // console.log(getComputerChoice());
 
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    return "It's a tie!";
-  } else if (humanChoice === "rock" && computerChoice === "scissors") {
-    return "You Win!  Rock crushes Scissors!";
-  } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    return "You Win!  Scissors cut through Paper!";
-  } else if (humanChoice === "paper" && computerChoice === "rock") {
-    return "You Win!  Paper covers Rock!";
-  } else if (humanChoice === "scissor" && computerChoice === "rock") {
-    return "You Lost! Scissors gets crushed by Rock!";
-  } else if (humanChoice === "paper" && computerChoice === "scissors") {
-    return "You Lost!  Paper gets cut by Scissors!";
-  } else if (humanChoice === "rock" && computerChoice === "paper") {
-    return "You Lost!  Rock gets covered by Paper!";
-  } else {
-    return "You lost because you entered an incorrect value.";
+function playGame(playRound, humanScore, computerScore) {
+  const humanScore = 0;
+  const computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      return "It's a tie!";
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+      return "You Win!  Rock crushes Scissors!";
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+      return "You Win!  Scissors cut through Paper!";
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+      return "You Win!  Paper covers Rock!";
+    } else if (humanChoice === "scissor" && computerChoice === "rock") {
+      return "You Lost! Scissors gets crushed by Rock!";
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+      return "You Lost!  Paper gets cut by Scissors!";
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+      return "You Lost!  Rock gets covered by Paper!";
+    } else {
+      return "You lost because you entered an incorrect value.";
+    }
   }
 }
-
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
