0259d8a90ab55323975518eff0e541a510aeb135
Brandon McInroy <brandon@codingbusdriver.ca>
2024-08-31 19:19:07 -0700

    Changed the "Win" string for human win

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,6 +1,12 @@
 const humanScore = 0;
 const computerScore = 0;
 
+function getHumanChoice() {
+  const humanChoice = prompt("Please choose Rock, Paper or Scissors");
+  return "You chose" + " " + humanChoice.toLowerCase() + ".";
+}
+console.log(getHumanChoice());
+
 function getComputerChoice() {
   const computerChoices = ["Rock", "Paper", "Scissors"];
   const computerChoice =
@@ -9,28 +15,22 @@ function getComputerChoice() {
 }
 console.log(getComputerChoice());
 
-function getHumanChoice() {
-  const humanChoice = prompt("Please choose Rock, Paper or Scissors");
-  return "You chose" + " " + humanChoice.toLowerCase() + ".";
-}
-
-console.log(getHumanChoice());
-
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
     return "It's a tie!";
-  } else if (
-    (humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "scissors" && computerChoice === "paper") ||
-    (humanChoice === "paper" && computerChoice === "rock")
-  ) {
-    return "You Win!";
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    return "You Win!  Rock crushes !";
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    return "You Win!  Scissors cut through Paper!";
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    return "You Win!  Paper covers Rock";
   } else {
-    return "You Loose!";
+    return "You Lost!";
   }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+const result = playRound(humanSelection, computerSelection);
+console.log(result);
